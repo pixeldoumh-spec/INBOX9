58f8299c97b174f0af79f986e69b2f4d835421de
pixeldoumh-spec <pixeldoumh@gmail.com>
2026-09-24 11:58:45 +0530

    remove: delete Proxnum provider registry wiring

--- api/_lib/provider-registry.js
+++ api/_lib/provider-registry.js
@@ -1,11 +1,9 @@
 import { syntheticProvider } from './synthetic-provider.js';
 import { numberOtpProvider } from './numberotp-provider.js';
-import { proxnumProvider } from './proxnum-provider.js';
 
 const adapters = new Map([
   ['synthetic', syntheticProvider],
   ['numberotp', numberOtpProvider],
-  ['proxnum', proxnumProvider],
 ]);
 
 export function getProviderAdapter(adapterKey = 'synthetic') {
