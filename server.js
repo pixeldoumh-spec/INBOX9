868c1a7069e662d44f585cae66f7298d3f02a0b4
pixeldoumh-spec <pixeldoumh@gmail.com>
2026-09-24 12:54:02 +0530

    refactor: serve customer architecture modules

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ const staticFiles = new Map([
   ['/index.html', ['index.html', 'text/html; charset=utf-8']],
   ['/app.js', ['app.js', 'text/javascript; charset=utf-8']],
   ['/boot.js', ['boot.js', 'text/javascript; charset=utf-8']],
-  ['/styles.css', ['styles.css', 'text/css; charset=utf-8']]
+  ['/styles.css', ['styles.css', 'text/css; charset=utf-8']],
+  ['/customer/state.js', ['customer/state.js', 'text/javascript; charset=utf-8']],
+  ['/customer/api-client.js', ['customer/api-client.js', 'text/javascript; charset=utf-8']],
+  ['/customer/ui.js', ['customer/ui.js', 'text/javascript; charset=utf-8']]
 ]);
 
 class RuntimeResponse {
