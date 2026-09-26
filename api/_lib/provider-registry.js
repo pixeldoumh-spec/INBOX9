e9017bc0dbce1d712ee7e7583ea3c12e1416b060
pixeldoumh-spec <pixeldoumh@gmail.com>
2026-09-26 20:35:48 +0530

    Register vetted external provider adapters

--- api/_lib/provider-registry.js
+++ api/_lib/provider-registry.js
@@ -1,7 +1,13 @@
+import { asmsProvider } from './asms-provider.js';
+import { pvapinsProvider } from './pvapins-provider.js';
+import { smsVerificationNumberProvider } from './sms-verification-number-provider.js';
 import { syntheticProvider } from './synthetic-provider.js';
 
 const adapters = new Map([
   ['synthetic', syntheticProvider],
+  ['asms', asmsProvider],
+  ['pvapins', pvapinsProvider],
+  ['sms-verification-number', smsVerificationNumberProvider],
 ]);
 
 export function getProviderAdapter(adapterKey = 'synthetic') {
